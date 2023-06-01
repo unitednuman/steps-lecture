b94d4cf4002cae878c6add008bc9b8bdb6ae9d67
Numan Aslam <unitednuman@gmail.com>
2023-06-01 18:41:19 +0500

    lecture 3

--- main/main/settings.py
+++ main/main/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-&(8o2hx2w_(y&lacle0pr#btabilxs@k^od(h$gpn5^e(-@6^_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
     'blog_service'
 ]
@@ -46,6 +47,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
